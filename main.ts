0d42ae6129ec28bbdd494d4b7262773bc0440070
Tamaramt <99643663+Tamaramt@users.noreply.github.com>
2022-03-10 09:50:07 +0100

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,21 @@
-
+let variable = 0
+basic.forever(function () {
+    let distancia = 0
+    variable = maqueen.Ultrasonic(PingUnit.Centimeters)
+    basic.showString("distancia")
+    if (distancia < 60) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 15)
+        basic.pause(5000)
+    }
+    if (distancia < 10) {
+        maqueen.motorStop(maqueen.Motors.All)
+        basic.pause(500)
+        basic.showLeds(`
+            # . # . #
+            . # # # .
+            # # # # #
+            . # # # .
+            # . # . #
+            `)
+    }
+})
